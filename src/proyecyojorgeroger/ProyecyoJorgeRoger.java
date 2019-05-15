3d543b59433a6b21d68ae1f7e5fb40e78b21233c
AlumGS <AlumGS@PCProfesor>
2019-05-15 18:37:08 +0200

    yo

--- src/proyecyojorgeroger/ProyecyoJorgeRoger.java
+++ src/proyecyojorgeroger/ProyecyoJorgeRoger.java
@@ -4,7 +4,7 @@ package proyecyojorgeroger;
 public class ProyecyoJorgeRoger {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola rogger");
     }
     
 }
