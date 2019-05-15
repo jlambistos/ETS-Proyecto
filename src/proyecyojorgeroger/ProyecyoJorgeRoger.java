1deea057fd73255d6f55e260c5dcda9ad6852379
rogge <rogge@LAPTOP-4O7FHSB7>
2019-05-15 18:38:56 +0100

    inicio

--- src/proyecyojorgeroger/ProyecyoJorgeRoger.java
+++ src/proyecyojorgeroger/ProyecyoJorgeRoger.java
@@ -6,6 +6,7 @@ public class ProyecyoJorgeRoger {
     public static void main(String[] args) {
         System.out.println("hola rogger");
         System.out.println("hola jorge, ¿C{omo estás?");
+        System.out.println("jajajajaj ");
     }
     
 }
