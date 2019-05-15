c3809250c8dcf5d5fa6ddd85632b32318f6e063c
Jorge <Jorge@pc-lambistos>
2019-05-15 20:45:37 +0100

    Creación de matriz y solicitud de datos por pantalla

--- src/proyecyojorgeroger/ProyecyoJorgeRoger.java
+++ src/proyecyojorgeroger/ProyecyoJorgeRoger.java
@@ -1,14 +1,35 @@
-
 package proyecyojorgeroger;
+
+import javax.swing.JOptionPane;
 
 public class ProyecyoJorgeRoger {
 
+    //Variables Globales
+    static int n;
+
     public static void main(String[] args) {
-        System.out.println("hola rogger");
-        System.out.println("hola jorge, ¿C{omo estás?");
-        System.out.println("jajajajaj ");
-        System.out.println("jejejejej");
-        
+        String ax = JOptionPane.showInputDialog("Ingrese el tamanio de la matriz: ");
+        JOptionPane.showMessageDialog(null, "El numero ingresado es: " + ax);
+        n = Integer.parseInt(ax);//Se convierte el dato a entero
+        String valor1 = JOptionPane.showInputDialog("Ingrese los valores que desea que se muestren en la matriz, Primer valor o valor Externo de la matriz: ");
+
+        String valor2 = JOptionPane.showInputDialog("Ingrese Segundo valor o valor Interno de la matriz: ");
+        JOptionPane.showMessageDialog(null, "Los valores son: " + valor1 + " Externo y " + valor2 + " Interno.");
+
+        for (int x = 0; x < n; x++) {
+
+            for (int y = 0; y < n; y++) {
+                if (x == 0 || y == 0 || x == n - 1 || y == n - 1) {
+                    System.out.print(valor1);
+                    //System.out.print("1");
+                } else {
+                    System.out.print(valor2);
+                    //System.out.print("0");
+                }
+            }
+            System.out.println("");
+        }
+
     }
-    
+
 }
